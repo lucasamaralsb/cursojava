96de4a506bce8ecda95869295de2172ca749b7c0
lucas amaral <lucasamaral.tec@gmail.com>
2021-03-21 12:45:34 -0300

    Segundo arquivo

---
+++ Tarefa.java
@@ -0,0 +1,18 @@
+package modulo1;
+
+public class Tarefa {
+
+	public static void main(String[] args) {
+	
+		int contador = 1;
+		double somador =0;
+		
+		for (;contador<=100; contador++) {
+			somador++;
+			
+			
+		}
+		System.out.println(somador);
+	}
+
+}
